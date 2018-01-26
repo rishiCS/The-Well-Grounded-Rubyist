df040de5cfd9dcebedb202de96f7a1fbcd76529d
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-26 23:23:55 +0530

    Create listing_4.4.rb

---
+++ Ch4/listing_4.4.rb
@@ -0,0 +1,16 @@
+# Nonmoduler rewrite of the Stack class
+
+class Stack
+ attr_reader  :stack
+ def initialize
+ @stack = []
+end
+
+def add_to_stack(obj)
+ @stack.push(obj)
+end
+
+def take_from_stack
+ @stack.pop
+ end
+end

c9055415fddc7006b50583269c2fb1e50633b147
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-27 17:28:13 +0530

    Delete listing_1.4.rb

--- listing_1.4.rb
+++
@@ -1,9 +0,0 @@
-# Tempurature converter with file output
-
-print "Hello. Please enter a Celsius value: "
-celsius = gets.to_i
-fahrenheit = (celsius *9 / 5) + 32
-puts "Saving result to output file 'temp.out'"
-fh= File.new("temp.out", "w")
-fh.puts fahrenheit
-fh.close

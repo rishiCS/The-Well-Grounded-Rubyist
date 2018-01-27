0a2f4094cb8e62a35bb324c20430194eb973e53f
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-27 18:26:12 +0530

    Create listing_5.4.rb

---
+++ Ch5/listing_5.4.rb
@@ -0,0 +1,22 @@
+# Reusing a variable name in nested local scopes
+
+class C
+ a = 5
+ module M
+   a = 4
+   module N
+     a = 3
+     class D
+      a = 2
+      def show_a
+       a = 1
+       puts a
+     end
+     puts a
+   end
+   puts a
+ end
+ puts a
+ end
+ puts a
+ end

b4ee65077b6302d62a589a916c8f227ef0a6ea3b
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-27 18:33:58 +0530

    Create listing_5.5.rb

---
+++ Ch5/listing_5.5.rb
@@ -0,0 +1,20 @@
+# Demonstarting the generation of a new local scope per method call
+
+class C
+  def x(value_for_a,recurse=false)
+  a = value_for_a
+  print "Here's the inspect-string for 'self' :"
+  p self
+  puts "And here's a:"
+  puts a
+  if recurse
+     puts "Calling myself (recursion)..."
+     x("Second value for a")
+     puts "Back after recursion; here's a:"
+     puts a
+  end
+ end
+end
+
+c = C.new
+c.x("First value for a", true)

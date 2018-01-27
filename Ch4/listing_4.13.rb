4c1001cddd4a6bd6b6a7a25839a61136937ac3c6
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-27 13:06:28 +0530

    Create listing_4.13.rb

---
+++ Ch4/listing_4.13.rb
@@ -0,0 +1,17 @@
+# Full Implementation of Person.method_missing
+
+def self.method_missing(m, *args)
+ method = m.to_s
+ if method.start_with?("all_with_")
+  attr = method[9..-1]
+  if self.public_method_defined?(attr)
+   PEOPLE.find_all do |person|
+    person.send(attr).include?(args[0])
+   end
+  else
+    raise ArgumentError, "Can't find #{attr}"
+    end
+  else
+   super
+  end
+ end

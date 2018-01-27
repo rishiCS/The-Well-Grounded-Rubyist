186be7641dc0e67d8662ef86f804a603123c4d2f
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-27 17:17:37 +0530

    Create listing_1.3.rb

---
+++ Ch1/listing_1.3.rb
@@ -0,0 +1,9 @@
+# Temperature converter using file input
+
+puts "Reading Celsius temperature value from data file...."
+num = File.read("temp.dat")
+celsius = num.to_i
+fahrenheit = (celsius * 9 /5 ) + 32
+puts "The number is " + num
+print "Result:"
+puts fahrenheit

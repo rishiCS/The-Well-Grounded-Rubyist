37aeacc8ac37d75160c05285cdf0589786d5f2d1
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-30 23:29:19 +0530

    Create looping_in_list.rb

---
+++ Ch6/looping_in_list.rb
@@ -0,0 +1,7 @@
+# Looping  based on a list of values
+
+celsius = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+puts "Celsius\tFahrenheit"
+for c in celsius
+  puts "#{c}\t#{Temperature.c2f(c)}"
+ends

cc9b9fafba6c681d962f976042d2acb148b742ee
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-30 23:42:55 +0530

    Create times.rb

---
+++ Ch6/times.rb
@@ -0,0 +1,12 @@
+# Implementing Times
+
+#The times method is an instance method of the Integer class, which means you call it as a method on integers.
+
+>>5.times { puts "Writing this 5 times!"}
+Writing this 5 times!
+Writing this 5 times!
+Writing this 5 times!
+Writing this 5 times!
+Writing this 5 times!
+=> 5
+

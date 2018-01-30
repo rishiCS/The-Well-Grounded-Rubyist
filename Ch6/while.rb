35da94bbb0bfe87c2af6d0d92066ec8ad1bffbf1
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-30 23:16:36 +0530

    Create while.rb

---
+++ Ch6/while.rb
@@ -0,0 +1,18 @@
+# The While Keyword
+
+n = 1
+while n < 11
+  puts n
+  n = n + 1
+end
+puts "Done!"
+
+
+# While at the end of the loop
+
+n = 1
+begin
+  puts n
+  n = n +1
+end while n < 11
+puts "Done!"

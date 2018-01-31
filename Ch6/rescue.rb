b39e0eb181cfe43582b9a22dfc427554b84365d3
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-01-31 22:22:10 +0530

    Create rescue.rb

---
+++ Ch6/rescue.rb
@@ -0,0 +1,12 @@
+# The rescue keyword to the rescue!
+
+print "Enter a number: "
+n = gets.to_i
+begin
+  result = 100 / n
+rescue
+  puts "Your number didn't work. Was it zero???"
+  exit
+end
+
+puts "100/#{n} is #{result}."

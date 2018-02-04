a77acec0db7efb4c92a4b8cca0c84a43633aeecb
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-02-04 13:45:33 +0530

    Create listing_7.1.rb

---
+++ Ch7/listing_7.1.rb
@@ -0,0 +1,27 @@
+#Testing the Boolean value of expressions using if constructs
+
+if (class Myclass; end)
+  puts "Empty class definition is true!"
+else
+  puts "Empty class Definition is false!"
+end
+if (class Myclass; 1; end)
+  puts "Class definition with the number 1 in it is true!"
+else
+  puts "Class definition with the number 1 in it is false!"
+end
+if (def m; return false; end)
+  puts "Method definition is true!"
+else
+  puts "Method definition is false!"
+end
+if "string"
+  puts "Strings appear to be true!"
+else
+  puts "Strings appear to be false!"
+end
+if 100 > 50
+  puts "100 is greater than 50!"
+else
+  puts "100 is not greater than 50!"
+end

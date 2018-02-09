0e5b6f9bdac3c5e8250814e050b2ed6c97ef46b8
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-02-09 16:54:11 +0530

    Create combining_strings.rb

---
+++ Ch8/combining_strings.rb
@@ -0,0 +1,35 @@
+# Combining Strings
+
+>> "a" +  "b"
+=> "ab"
+
+>> "a" + "b" + "c"
+=> "abc"
+
+>> str = "Hi"
+=> "Hi"
+
+>> str + "there."
+=> "Hi there."
+
+>>str
+=> "Hi"
+
+str = "Hi"
+=> "Hi"
+
+str << "there."
+=> "Hi there."
+
+>> str
+=> "Hi there."
+
+
+# String combination via interpolation
+
+str = "Hi"
+=> "Hi"
+
+"#{str} there."
+=> "Hi there."
+

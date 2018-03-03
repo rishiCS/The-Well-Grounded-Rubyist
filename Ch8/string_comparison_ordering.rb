0fee9bbc0903fd293d2ed03ceeb4275820e97703
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-03-03 12:07:17 +0530

    Create string_comparison_ordering.rb

---
+++ Ch8/string_comparison_ordering.rb
@@ -0,0 +1,29 @@
+>> "a" <=> "b"
+=> -1
+
+>> "b" > "a"
+=> true
+
+>> "a" > "A"
+=>true
+
+>> "." > ","
+=> true
+
+
+# Comparing strings
+
+>> "string" == "string"
+=> true
+
+>> "string" == "house"
+=> false
+
+>> "a" == "a"
+=> true
+
+>> "a".equal?("a")
+=> false
+
+>> "a".equal?(100)
+=>false

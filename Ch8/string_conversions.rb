4b56905862e7190f8e0a80d7f4795ac1e29ff82f
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-03-03 12:41:17 +0530

    Create string_conversions.rb

---
+++ Ch8/string_conversions.rb
@@ -0,0 +1,23 @@
+>> "100".to_i(17)
+=> 289
+
+>>"100".oct
+=>64
+
+>>"100".hex
+=>256
+
+>>"1.2345".to_f
+=>1.2345
+
+>>"Hello".to_s
+=> "Hello"
+
+>> "abcde".to_sym
+=>:abcde
+
+>>"1.2345and some words".to_f
+=> 1.2345
+
+>> "just some words".to_i
+=>0

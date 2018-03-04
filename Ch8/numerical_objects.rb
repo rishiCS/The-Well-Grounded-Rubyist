b3cbfebcc4196f6f0aa44ea5a2530ce38b35db98
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-03-04 18:21:23 +0530

    Create numerical_objects.rb

---
+++ Ch8/numerical_objects.rb
@@ -0,0 +1,13 @@
+# In Ruby, numbers are objects.
+
+n = 99.6
+m = n.round
+puts m
+x = 12
+if x.zero?
+  puts "x is zero "
+else
+  puts "x is not zero"
+end
+
+puts "The ASCII character equivalent of 97 is #{97.chr}"

2f94066695f1eb41c85cd5676c004e89c7eb0210
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-03-04 18:27:53 +0530

    Create times_dates.rb

---
+++ Ch8/times_dates.rb
@@ -0,0 +1,7 @@
+>> require 'date'
+=> true
+
+>>Date.parse("April 24 1997").england.strftime("%B %d %Y")
+=> "April 24 1997"
+
+# Times and Dates are manipulated through three classes: Time, Date, and DateTime.

bc1cc8e647fc6c434962ceabe707e6920f2f5292
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-03-04 18:12:20 +0530

    Create symbols.rb

---
+++ Ch8/symbols.rb
@@ -0,0 +1,16 @@
+# Symbols and their uses
+
+:a
+:book
+: "Here's how to make a symbol with spaces in it."
+
+# to_sym method
+
+>> "a".to_sym
+=>:a
+
+>>"Converting string to symbol with intern...".intern
+>>"Converting string to symbol with intern..."
+
+>>:a.to_s
+=> "a"

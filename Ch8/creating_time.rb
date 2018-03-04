aa57e26f8f1eab95af0943e0b08595700b2f01ba
Rishikesh Bamdale <30471743+rishiCS@users.noreply.github.com>
2018-03-04 18:40:02 +0530

    Create creating_time.rb

---
+++ Ch8/creating_time.rb
@@ -0,0 +1,9 @@
+>> puts DateTime.new(2009, 1, 2, 3, 4, 5)
+2009-01-02T03:04:05+00:00
+
+>> puts DateTime.now
+2013-11-03T04:44:52-008:00
+=>nil
+
+>> puts DateTime.parse("October 23, 1973, 10:34 AM")
+1973-10-23T10:34:00+00:00
